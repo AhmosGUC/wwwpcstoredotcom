5b124736940e68146aee69229753875501bfd267
HeshamAhmos <hesham.ahmos.y@gmail.com>
2021-05-26 01:10:39 +0200

    loading csv files on start

--- backend/index.js
+++ backend/index.js
@@ -6,15 +6,26 @@ const HOST = '0.0.0.0';
 const express = require('express');
 var cors = require('cors')
 var bodyparser = require('body-parser')
-
+const csv = require('csv-parser');
+const fs = require('fs');
 // App
 const app = express();
 app.use(cors());
 app.use(bodyparser.json());
 
+fs.createReadStream('datastore/dataset.csv')
+    .pipe(csv())
+    .on('data', (row) => {
+        
+    })
+    .on('end', () => {
+        console.log('CSV file successfully processed');
+    });
 
 app.get('/', (req, res) => {
-    res.json({'message':'Hello World'});
+    res.json({
+        'message': 'Hello World'
+    });
 });
 
 app.listen(PORT, HOST);
