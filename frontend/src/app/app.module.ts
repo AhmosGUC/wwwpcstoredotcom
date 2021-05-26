3c0772de4ff9f79b132ccc1521c297242e57758c
HeshamAhmos <hesham.ahmos.y@gmail.com>
2021-05-26 18:14:26 +0200

    computer class, generate computerlist compounent

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ComputerListComponent } from './computer-list/computer-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComputerListComponent
   ],
   imports: [
     BrowserModule
---
+++ frontend/src/app/computer.ts
@@ -0,0 +1,16 @@
+export class Computer {
+    constructor(
+        public company: string,
+        public product: string,
+        public type: string,
+        public inches: Number,
+        public Resolution: string,
+        public cpu: string,
+        public ram: string,
+        public memory: string,
+        public graphics: string,
+        public opsys: string,
+        public weight: string,
+        public price: Number,
+    ) { }
+}
