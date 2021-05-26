c186e9466d07cdf05996aec5d3baab141886c485
HeshamAhmos <hesham.ahmos.y@gmail.com>
2021-05-26 02:13:16 +0200

    REST GET Endpoints

--- backend/index.js
+++ backend/index.js
@@ -18,17 +18,17 @@ app.use(bodyparser.json());
 async function initConn() {
     try {
         await db.authenticate();
-        console.log('Connection has been established successfully.');
+        console.log('Connection to database (Memory) has been established successfully.');
     } catch (error) {
-        console.error('Unable to connect to the database:', error);
+        console.error('Unable to connect to the database (Memory):', error);
     }
 }
 async function createTable() {
     try {
         await Computer.sync()
-        console.log("Created")
+        console.log("Computers table created.")
     } catch (error) {
-        console.log("error", error);
+        console.log("Error creating Computers table", error);
     }
 }
 initConn()
@@ -42,18 +42,87 @@ initConn()
                     });
                 })
                 .on('end', () => {
-                    console.log('CSV file successfully processed');
+                    console.log('CSV file successfully processed and loaded to database (Memory).');
                 });
         });
     })
 
 
 
-app.get('/', (req, res) => {
-    res.json({
-        'message': 'Hello World'
+app.get('/computers', (req, res) => {
+    const limit = req.query.limit || 10
+    const page = req.query.page || 1
+    const offset = (page - 1) * limit;
+    Computer.count().then((amount) => {
+        Computer.findAll({
+            offset: offset,
+            limit: limit
+        }).then((result) => {
+            res.json({
+                result: result,
+                count: limit,
+                totalCount: amount
+            });
+        })
+    })
+});
+
+app.get('/computers/cpu', (req, res) => {
+    Computer.aggregate('CPU', 'DISTINCT', {
+        plain: false
+    }).then((result) => {
+        let data = [];
+        result.forEach(element => {
+            data.push(element['DISTINCT']);
+        });
+        res.json({
+            'count': data.length,
+            'cpus': data
+        });
+    });
+});
+app.get('/computers/ram', (req, res) => {
+    Computer.aggregate('RAM', 'DISTINCT', {
+        plain: false
+    }).then((result) => {
+        let data = [];
+        result.forEach(element => {
+            data.push(element['DISTINCT']);
+        });
+        res.json({
+            'count': data.length,
+            'rams': data
+        });
+    });
+});
+app.get('/computers/opsys', (req, res) => {
+    Computer.aggregate('OpSys', 'DISTINCT', {
+        plain: false
+    }).then((result) => {
+        let data = [];
+        result.forEach(element => {
+            data.push(element['DISTINCT']);
+        });
+        res.json({
+            'count': data.length,
+            'opsys': data
+        });
+    });
+});
+app.get('/computers/inches', (req, res) => {
+    Computer.aggregate('Inches', 'DISTINCT', {
+        plain: false
+    }).then((result) => {
+        let data = [];
+        result.forEach(element => {
+            data.push(element['DISTINCT']);
+        });
+        res.json({
+            'count': data.length,
+            'inches': data
+        });
     });
 });
 
 app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+console.log(`Running on http://${HOST}:${PORT}.`);
